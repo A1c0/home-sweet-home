be88f4c0857b65813e11ddeb8524810fe29575b7
Alexandre CONTASSOT <alexandrecontassot@hotmail.fr>
2023-03-22 16:51:23 +0100

    add brew

---
+++ brew-install.sh
@@ -0,0 +1,4 @@
+# config: https://github.com/dandavison/delta
+brew install awscli gh volta thefuck git git-delta topgrade
+
+brew install --cask boom-3d 1password spotify cleanmymac alfred rectangle-pro joplin slack jetbrains-toolbox michaelvillar-timer hyper docker scroll-reverser cakebrew devtoys
